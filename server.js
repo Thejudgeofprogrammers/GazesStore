c0bec4f1777a51b1fb300dcd287d557602de636a
Thejudgeofprogrammers <temka2004@hotmail.com>
2024-04-14 01:33:14 +0300

    content

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const router = require('./src/routers/router.js');
+const router = require('./src/routers/router');
+const logger = require('./middleware/logger');
+const error404 = require('./middleware/error-404');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'public', 'static'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use('/', router);
+app.use(logger);
+app.use(error404);
 
 const PORT = process.env.PORT ?? 3030;
 
